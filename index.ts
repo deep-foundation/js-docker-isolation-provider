0c424f668c872ff667f4b6232672c51379a20a12
konard <drakonard@gmail.com>
2021-12-24 10:53:05 +0300

    A better error handling.

--- index.ts
+++ index.ts
@@ -8,19 +8,35 @@ app.get('/healthz', (req, res)=>{
   res.json('ok');
 });
 app.post('/init', (req, res)=>{
-  console.log('req?.body', req?.body);
-  initiated = eval(req?.body?.params?.code);
-  console.log('initiated', initiated);
-  res.json('ok');
+  try 
+  {
+    console.log('req?.body', req?.body);
+    initiated = eval(req?.body?.params?.code);
+    console.log('initiated', initiated);
+    res.json('ok');
+  }
+  catch(error)
+  {
+    console.log('error', JSON.stringify(error, null, 2));
+    res.json('error');
+  }
 });
 app.post('/call', (req, res)=>{
-  // typeof initiated === 'function' ? res.json(initiated(req.body)) : res.json({error: 'init error'});
-  if (typeof initiated !== 'function') res.json({error: 'init error'});
-  const result = initiated(req.body);
-  console.log('call result', result);
-  res.json(result);
+  try 
+  {
+    // typeof initiated === 'function' ? res.json(initiated(req.body)) : res.json({error: 'init error'});
+    if (typeof initiated !== 'function') res.json({ rejected: { error: 'init error' }});
+    const result = initiated(req.body);
+    console.log('call result', result);
+    res.json({ resolved: result });
+  }
+  catch(error)
+  {
+    console.log('error', JSON.stringify(error, null, 2));
+    res.json({ rejected: { error: error?.message ?? error }});
+  }
 });
   
 app.listen(process.env.PORT, () => {
-  console.log(`Hello bugfixers! Listening ${process.env.PORT} port`);
+  console.log(`Listening ${process.env.PORT} port`);
 })
